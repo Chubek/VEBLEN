b774eaa0e3d387fb5d47023957d115ee58bd3566
Chubak Bidpaa <chubakbidpaa@gmail.com>
2024-06-23 14:39:46 +0330

    Re-do: The inter is being revamped

--- Inter.h
+++ Inter.h
@@ -16,44 +16,51 @@
 #define INTER_REALLOC(mem, nn) __inter_heap_realloc(mem, nn)
 #define INTER_FREE(mem) __inter_heap_free(mem)
 
-struct INTER_LambdaSexp;
-struct INTER_String;
-struct INTER_Value;
-
-struct INTER_String {
-    uint8_t *buffer;
-    size_t buf_length;
-
-    struct INTER_String *next;
-};
-
-struct INTER_Value {
+struct INTER_Expr {
    enum {
-	INTERVAL_String,
-	INTERVAL_Number,
-	INTERVAL_Argument,
+	EXP_Constant,
+	EXP_Variable,
+	EXP_Application,
+	EXP_Abstraction,
+	EXP_Let,
+	EXP_Letrec,
+	EXP_LetIn,
+	EXP_Infix,
+	EXP_Case,
+	EXP_Pattern,
+	EXP_Cons,
    } kind;
 
    union {
-   	INTER_String *v_string;
-	long double v_number;
+	struct INTER_Const *v_const;
+	struct INTER_Variable *v_variable;
+	struct INTER_Application *v_application;
+	struct INTER_Abstraction *v_abstraction;
+	struct INTER_Let *v_let;
+	struct INTER_LetRec *v_letrec;
+	struct INTER_LetIn *v_letin;
+	struct INTER_Infix *v_infix;
+	struct INTER_Case *v_case;
+	struct INTER_Pattern *v_pattern;
+	struct INTER_Cons *v_cons;
    };
-
-   struct INTER_Value *next;
 };
 
-struct INTER_LambdaSexp {
-    struct INTER_String *name;
-
-    enum {
-	LAMSEXP_Atom,
-	LAMSEXP_List,
-    } kind;
-
-    struct INTER_Value *value;
+struct INTER_Const {
+   uint8_t repr[MAX_CONST_REPR + 1];
+   enum {
+	CNR_Number,
+	CNR_String,
+	CNR_Operator,
+	CNR_Intrin,
+	CNR_Ident,
+	/* TODO: Add more */
+   } kind;
 };
 
-
-
+struct INTER_Variable {
+    uint8_t repr[MAX_VARIABLE_REPR + 1];
+    struct INTER_Symtab **static_link;
+};
 
 #endif
