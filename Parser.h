fe1f6eddb75761f510349b1822668f8fd32eb2ea
Chubak Bidpaa <chubakbidpaa@gmail.com>
2024-06-25 10:09:47 +0330

    Added: The EBNF grammar (for design specs)

---
+++ Parser.h
@@ -0,0 +1,16 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+#include <string.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+
+
+
+
+#endif
